a554fa9e6499da8dd432197b256af856fdea4ecf
Felipe Santarosa <snowychest@gmail.com>
2021-05-25 23:30:25 -0300

    fix: remove commands from root index.js

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
+client.on('message', require('./src/commands/index'));
 
 client.login(process.env.BOT_TOKEN);
