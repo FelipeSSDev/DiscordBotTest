995005fc678bbc65b9bc12a9832495f926630a14
Felipe Santarosa <snowychest@gmail.com>
2021-05-26 04:34:07 -0300

    feat: Complete function to get a random pokemon

--- src/commands/pokemon.ts
+++ src/commands/pokemon.ts
@@ -1,20 +1,49 @@
-import {Message} from 'discord.js';
+import {Message, MessageEmbed} from 'discord.js';
+import axios from 'axios';
+import generateRandomNumber from '../utils/randomNumber';
+
+interface TypeInfo {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
 
 export default (message: Message, args: string[]) => {
-    const commands: {[key: string]: () => {}} = {
-        getRandom: async () => {
-            message.channel.send('salve random');
-        },
-        getPikachu: async () => {
-            message.channel.send('salve pikachu');
-        },
-    };
+  const commands: {[key: string]: () => {}} = {
+    ['get-random']: async () => {
+      try {
+        const randomNumber = generateRandomNumber(1, 152);
+        const {data: response} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
 
-    for (let key in commands) {
-        if (args[0] !== key) continue;
+        const pokemonTypes = response.types.map((typeInfo: TypeInfo) => ({
+          name: `Type ${typeInfo.slot}`,
+          value: typeInfo.type.name,
+          inline: true,
+        }));
 
-        return commands[key]();
-    }
+        const pokemonEmbed = new MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(response.name)
+          .setThumbnail(response.sprites.other['official-artwork'].front_default)
+          .addFields(...pokemonTypes, {name: 'Pokemon ID', value: response.id})
+          .setTimestamp()
+          .setFooter('Top 10 Rogers da Internet', 'https://avatars.githubusercontent.com/u/80218280?v=4');
 
-    return message.channel.send('Argumento não suportado pelo comando');
+        message.channel.send(pokemonEmbed);
+      } catch (e) {
+        console.log(e);
+        message.channel.send('Não consegui encontrar um pokemon para você');
+      }
+    },
+  };
+
+  for (let key in commands) {
+    if (args[0] !== key) continue;
+
+    return commands[key]();
+  }
+
+  return message.channel.send('Argumento não suportado pelo comando');
 };
