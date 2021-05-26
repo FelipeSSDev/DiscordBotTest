c488992dc87439b3e2e09d519836230d4519c862
Felipe Santarosa <snowychest@gmail.com>
2021-05-25 21:56:22 -0300

    fix: prettier update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-require('dotenv').config()
+require('dotenv').config();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
